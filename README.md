48f9e985d7e157a159e74bee98b1e37126e1a052
Dan Thompson <d.thompso@yahoo.com>
2018-05-06 10:23:28 -0400

    a few tests

--- README.md
+++ README.md
@@ -1,9 +1,16 @@
 # DinDin API
 ### REST API for DinDIn storage
 
-## Dev setup
+## Dev Setup
 ```buildoutcfg
 python manage.py migrate
 demodata.bat
 python manage.py runserver
+```
+
+## Generating Test Data
+###dumping new fixtures from a database
+```buildoutcfg
+python manage.py dumpdata meals --format=json --indent=4 > ./meals/fixtures/mealsdump.json
+python manage.py dumpdata auth --format=json --indent=4 > ./meals/fixtures/authdump.json
 ```
---
+++ meals/tests/integrationtest.py
@@ -0,0 +1,49 @@
+from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+from django.contrib.auth.models import User
+from meals.views import MealList
+
+
+class MealsTest(TestCase):
+    fixtures = ['mealsdump.json', 'authdump.json']
+
+    def test_get_meals_returns_200_status(self):
+        view = MealList.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('meals', format='json')
+        user = User.objects.get(username='test')
+        force_authenticate(request, user=user)
+        response = view(request)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_text, 'OK')
+
+    def test_get_meals_returns_a_meals_list(self):
+        view = MealList.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('meals', format='json')
+        user = User.objects.get(username='test')
+        force_authenticate(request, user=user)
+        response = view(request)
+        data = response.data
+
+        for meal in data:
+            self.assertIn("name", meal)
+            self.assertIn("owner", meal)
+            self.assertIn("taste", meal)
+            self.assertIn("difficulty", meal)
+            self.assertIn("last_used", meal)
+            self.assertIn("used_count", meal)
+            self.assertIn("notes", meal)
+
+    def test_get_meals_returns_meals_for_specified_user(self):
+        view = MealList.as_view()
+        factory = APIRequestFactory()
+        request = factory.get('meals', format='json')
+        user = User.objects.get(username='test')
+        force_authenticate(request, user=user)
+        response = view(request)
+        data = response.data
+
+        for meal in data:
+            self.assertEquals(meal.get("owner"), "test")
