3e688d769b987132d58c4b0c85641beb63c891f8
Dan Thompson <d.thompso@yahoo.com>
2018-05-09 09:23:19 -0400

    object level user access

--- meals/views.py
+++ meals/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions
 from meals.models import Meal
 from meals.serializers import MealSerializer
@@ -11,11 +12,26 @@ class MealList(generics.ListCreateAPIView):
         user = self.request.user
         return Meal.objects.filter(owner=user)
 
+    def get_object(self):
+        key = self.kwargs["pk"]
+        obj = get_object_or_404(self.get_queryset(), pk=key)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
 
 class MealDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Meal.objects.all()
     serializer_class = MealSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        key = self.kwargs["pk"]
+        return Meal.objects.filter(pk=key)
+
+    def get_object(self):
+        user = self.request.user
+        obj = get_object_or_404(self.get_queryset(), owner=user)
+        self.check_object_permissions(self.request, obj)
+        return obj
