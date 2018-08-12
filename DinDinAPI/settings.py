6eab615ac6a803c314c5e83462545552a9a79b9f
Dan Thompson <d.thompso@yahoo.com>
2018-08-12 12:19:48 -0400

    try not whitenoise?

--- DinDinAPI/settings.py
+++ DinDinAPI/settings.py
@@ -168,11 +168,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
 # Enable static file compression
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.staticfiles_storage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
